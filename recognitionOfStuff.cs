78c71514a6e9a8b4dc505e3e5c144b27e86092dc
LevelNico <quellmoell@gmail.com>
2014-12-12 12:09:07 +0100

    Inserted sobel edge detection.
    Inserted sobel edge detection and deleted some commented out code.

--- recognitionOfStuff.cs
+++ recognitionOfStuff.cs
@@ -17,6 +17,8 @@ public class recognitionOfStuff : MonoBehaviour {
 	float[,,] pix2; //two images so that we don't end up with a rippling effect when applying values.
 	int w;
 	int h;
+	int[,] SobelYKernel = {{-1,0,1},{-2,0,2},{-1,0,1}};
+	int[,] SobelXKernel = {{1,2,1},{0,0,0},{-1,-2,-1}};
 
 	void Start(){
 		oldImage = (Texture2D)renderer.material.mainTexture;
@@ -27,12 +29,38 @@ public class recognitionOfStuff : MonoBehaviour {
 	}
 
 	void Update () {
-		/*Image<Bgr, byte> picture = new Image<Bgr, byte>("C:\\picture1.jpg");
-		Bgr myWhiteColor = new Bgr(255, 255, 255);
-		for (int i=0; i<200; i++){
-			picture[i,i]= myWhiteColor;
-		}    
-		picture.Save("C:\\picture2.jpg");*/
+		if (Input.GetKeyDown (KeyCode.O)){ //Sobel Edge Detector
+			pix2 = Color2Float (colors, w, h);
+			for (int y=1; y<h-1; y++) {
+				for (int x=1; x<w-1; x++) {
+					float tmp = pix2[y,x,0]+pix2[y,x,1]+pix2[y,x,2]/3;
+					for (int c=0; c<3; c++) {
+						pix2[y,x,c] = tmp;
+					}
+				}
+			}
+			for (int y=1; y<h-1; y++) {
+				for (int x=1; x<w-1; x++) {
+					float tmp = pix2[y,x,0]+pix2[y,x,1]+pix2[y,x,2]/3;
+					for (int c=0; c<3; c++) {
+						float sumy = 0f;
+						float sumx = 0f;
+						float sum = 0f;
+						for (int ky=-1; ky<=1; ky++) {
+							for (int kx=-1; kx<=1; kx++) {
+								
+								sumy += pix2[y+ky,x+kx,c]*SobelYKernel[ky+1,kx+1];
+								sumx += pix2[y+ky,x+kx,c]*SobelXKernel[ky+1,kx+1];
+							}//kx 
+						}//ky
+						sum = sumy+sumx;
+						pix[y,x,c] = sum;
+						
+						//}
+					}//c
+				}//x
+			}//y
+		}//if
 
 		pix = Color2Float (colors, w, h);
 
