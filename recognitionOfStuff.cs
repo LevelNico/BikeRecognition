ee7fc0fd4060fec5e1f314cf9a0c438708cf4e95
LevelNico <quellmoell@gmail.com>
2014-12-13 15:06:24 +0100

    Edge detection and threshold functional

--- recognitionOfStuff.cs
+++ recognitionOfStuff.cs
@@ -29,7 +29,10 @@ public class recognitionOfStuff : MonoBehaviour {
 	}
 
 	void Update () {
-		if (Input.GetKeyDown (KeyCode.O)){ //Sobel Edge Detector
+		pix = Color2Float (colors, w, h);
+
+
+		if (Input.GetKeyDown (KeyCode.R)){ //Sobel Edge Detector then Threshold
 			pix2 = Color2Float (colors, w, h);
 			for (int y=1; y<h-1; y++) {
 				for (int x=1; x<w-1; x++) {
@@ -55,16 +58,134 @@ public class recognitionOfStuff : MonoBehaviour {
 						}//ky
 						sum = sumy+sumx;
 						pix[y,x,c] = sum;
-						
-						//}
+					}//c
+				}//x
+			}//y
+			for (int y=0; y<h ; y++){
+				for (int x=0; x<w ; x++){
+					float tmpo = pix[y,x,0]+pix[y,x,1]+pix[y,x,2]/3;
+					for (int c=0; c<3 ; c++){
+						pix[y,x,c] = tmpo;
+						if (pix[y,x,c] > 127){
+							pix[y,x,c]= 255;
+						}
+						else{
+							pix[y,x,c] = 0;
+						}
 					}//c
 				}//x
 			}//y
 		}//if
 
-		pix = Color2Float (colors, w, h);
 
-	}
+		if (Input.GetKeyDown (KeyCode.E)) {      //Simple threshold then Sobel Edge
+			for (int y=0; y<h ; y++){
+				for (int x=0; x<w ; x++){
+					float tmpo = pix[y,x,0]+pix[y,x,1]+pix[y,x,2]/3;
+					for (int c=0; c<3 ; c++){
+						pix[y,x,c] = tmpo;
+						if (pix[y,x,c] > 127){
+							pix[y,x,c]= 255;
+						}
+						else{
+							pix[y,x,c] = 0;
+						}
+					}//c
+				}//x
+			}//y
+			pix2 = Color2Float (colors, w, h);
+			for (int y=1; y<h-1; y++) {
+				for (int x=1; x<w-1; x++) {
+					float tmp = pix2[y,x,0]+pix2[y,x,1]+pix2[y,x,2]/3;
+					for (int c=0; c<3; c++) {
+						pix2[y,x,c] = tmp;
+					}
+				}
+			}
+			for (int y=1; y<h-1; y++) {
+				for (int x=1; x<w-1; x++) {
+					float tmp = pix2[y,x,0]+pix2[y,x,1]+pix2[y,x,2]/3;
+					for (int c=0; c<3; c++) {
+						float sumy = 0f;
+						float sumx = 0f;
+						float sum = 0f;
+						for (int ky=-1; ky<=1; ky++) {
+							for (int kx=-1; kx<=1; kx++) {
+								
+								sumy += pix2[y+ky,x+kx,c]*SobelYKernel[ky+1,kx+1];
+								sumx += pix2[y+ky,x+kx,c]*SobelXKernel[ky+1,kx+1];
+							}//kx 
+						}//ky
+						sum = sumy+sumx;
+						pix[y,x,c] = sum;
+					}//c
+				}//x
+			}//y
+		}//if
+
+
+		if (Input.GetKeyDown (KeyCode.T)) {      //Only threshold
+			for (int y=0; y<h ; y++){
+				for (int x=0; x<w ; x++){
+					float tmpo = pix[y,x,0]+pix[y,x,1]+pix[y,x,2]/3;
+					for (int c=0; c<3 ; c++){
+						pix[y,x,c] = tmpo;
+						if (pix[y,x,c] > 127){
+							pix[y,x,c]= 255;
+						}
+						else{
+							pix[y,x,c] = 0;
+						}
+					}//c
+				}//x
+			}//y
+		}//if
+
+
+		if (Input.GetKeyDown (KeyCode.O)){ //Sobel Edge Detector only
+			pix2 = Color2Float (colors, w, h);
+			for (int y=1; y<h-1; y++) {
+				for (int x=1; x<w-1; x++) {
+					float tmp = pix2[y,x,0]+pix2[y,x,1]+pix2[y,x,2]/3;
+					for (int c=0; c<3; c++) {
+						pix2[y,x,c] = tmp;
+					}
+				}
+			}
+			for (int y=1; y<h-1; y++) {
+				for (int x=1; x<w-1; x++) {
+					float tmp = pix2[y,x,0]+pix2[y,x,1]+pix2[y,x,2]/3;
+					for (int c=0; c<3; c++) {
+						float sumy = 0f;
+						float sumx = 0f;
+						float sum = 0f;
+						for (int ky=-1; ky<=1; ky++) {
+							for (int kx=-1; kx<=1; kx++) {
+								
+								sumy += pix2[y+ky,x+kx,c]*SobelYKernel[ky+1,kx+1];
+								sumx += pix2[y+ky,x+kx,c]*SobelXKernel[ky+1,kx+1];
+							}//kx 
+						}//ky
+						sum = sumy+sumx;
+						pix[y,x,c] = sum;
+					}//c
+				}//x
+			}//y
+		}//if
+
+
+		if(Input.GetKeyDown (KeyCode.K)){ //Reset
+			colors = oldImage.GetPixels();
+			pix = Color2Float(colors,w,h);
+		}//if
+
+
+		colors = Float2Color (pix, w, h);
+		newImage.SetPixels(colors);
+		newImage.Apply();
+		renderer.material.mainTexture = newImage;
+
+	}//void update
 
 	float[,,] Color2Float(Color[] col, int w, int h){
 		float[,,] tempArr = new float[h, w, 3];
@@ -89,4 +210,4 @@ public class recognitionOfStuff : MonoBehaviour {
 		}
 		return tempArra;
 	}
-}
+}//recognitionOfStuff
